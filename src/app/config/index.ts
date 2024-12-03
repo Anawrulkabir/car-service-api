f4e940190bfa4781780ddde72a83eeb78fa29999
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-12-03 19:59:36 +0600

    Some unnecessary files removed

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,5 +6,4 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  base_url: process.env.BASE_URL,
 };
